53d79d0b4fc4a871629f27da345c5063208f333b
Felipe <felipetonim10gmail.com>
2025-02-05 00:14:20 -0300

    modification to profile image alt

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 function toggleMode() {
   const html = document.documentElement
-  html.classList.toggle('light')
+  html.classList.toggle("light")
   const img = document.querySelector("#profile img")
-  if (html.classList.contains('light')) {
-    img.setAttribute('src', './assets/avatar-light.png')
-  } else
-  img.setAttribute('src', './assets/avatar.png')
-}
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-light.png") (img.alt = "foto de Maik brito sorrindo de oculos escuro e fundo azul");
+  } else  img.setAttribute("src", "./assets/avatar.png", "alt, foto b") (img.alt = "foto de maik brito sorrindo com fundo amarelo");
+  }
