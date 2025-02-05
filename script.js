554eab2dde2019fd7f032a53ccdc21c7d421b0f0
Felipe <felipetonim10gmail.com>
2025-02-05 01:16:45 -0300

    modification to my links and avatar

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ function toggleMode() {
   html.classList.toggle("light")
   const img = document.querySelector("#profile img")
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/avatar-light.png") (img.alt = "foto de Maik brito sorrindo de oculos escuro e fundo azul");
-  } else  img.setAttribute("src", "./assets/avatar.png", "alt, foto b") (img.alt = "foto de maik brito sorrindo com fundo amarelo");
+    img.setAttribute("src", "./assets/avatar-light.png") (img.alt = "foto de Felipe Tonim com fundo roxo");
+  } else  img.setAttribute("src", "./assets/avatar.png", "alt, foto b") (img.alt = "foto de Felipe Tonim com fundo branco");
   }
